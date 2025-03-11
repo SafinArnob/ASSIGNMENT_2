c512c3bc34b08d1c630f5b3a5ee75051bd7593f3
Safin <arnobsafin2004@gmail.com>
2025-03-11 12:17:50 +0600

    assignment

---
+++ MAIN.js
@@ -0,0 +1,60 @@
+function calculateSum(a, b) 
+{
+    return a + b;
+}
+
+function isEVEN(a)
+{
+    if( a%2 === 0)
+    {
+        console.log("TRUE");
+    }
+    else{
+        console.log("FALSE");
+    }
+}
+
+function findMax(arr)
+{
+    return Math.max(...arr);
+}
+
+function filterOddNumbers(arr1)
+{
+    let oddNumbers = [];
+
+    for(let i=0; i<arr1.length; i++)
+    {
+        if(arr1[i]%2 !== 0)
+        {
+            oddNumbers.push(arr1[i]);
+        }
+    }
+    return oddNumbers;
+}
+
+function countWords(str) 
+{
+    const words = str.trim().split(/\s+/);
+    return words.length;
+}
+
+function removeDuplicates(arr2)
+{
+    const num = new Set();
+
+    for(let i=0; i<arr2.length; i++)
+    {
+        num.add(arr2[i]);
+    }
+
+    return [...num];
+}
+
+
+console.log("SUM OF TWO ELEMENT: " + calculateSum(10,10)); 
+isEVEN(7);
+console.log("MAXIMUM NUMBER IS: " + findMax([1,6,3,9,2,10]) )
+console.log("ODD NUMBERS ARRAY IS: " + filterOddNumbers([1,2,3,4,5,6,7,8,9]) )
+console.log("NUMBER OF WORDS IN THE SENTENCE: " + countWords("count the number words in the sentence")); 
+console.log("SET WITHOUT DUPLICATE NUMBER: " + removeDuplicates([1,2,3,1,4,2,5,7,4]))
